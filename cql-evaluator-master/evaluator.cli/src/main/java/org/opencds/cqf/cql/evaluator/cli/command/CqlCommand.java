a247cebb2147d91529e0dfb341e82a9b365c589c
Azhar <azhar0287@gmail.com>
2022-04-27 16:58:19 +0500

    patient json files removed and test jsonPatient data added

--- cql-evaluator-master/evaluator.cli/src/main/java/org/opencds/cqf/cql/evaluator/cli/command/CqlCommand.java
+++ cql-evaluator-master/evaluator.cli/src/main/java/org/opencds/cqf/cql/evaluator/cli/command/CqlCommand.java
@@ -1,16 +1,21 @@
 package org.opencds.cqf.cql.evaluator.cli.command;
 
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 import org.cqframework.cql.cql2elm.CqlTranslatorOptions;
 import org.cqframework.cql.cql2elm.CqlTranslatorOptionsMapper;
 import org.cqframework.cql.elm.execution.VersionedIdentifier;
 import org.hl7.fhir.instance.model.api.IBase;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
 import org.hl7.fhir.instance.model.api.IBaseDatatype;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.opencds.cqf.cql.engine.execution.EvaluationResult;
@@ -22,11 +27,13 @@ import org.opencds.cqf.cql.evaluator.builder.Constants;
 import org.opencds.cqf.cql.evaluator.builder.CqlEvaluatorBuilder;
 import org.opencds.cqf.cql.evaluator.builder.DataProviderFactory;
 import org.opencds.cqf.cql.evaluator.builder.EndpointInfo;
+import org.opencds.cqf.cql.evaluator.builder.data.TypedRetrieveProviderFactory;
 import org.opencds.cqf.cql.evaluator.cql2elm.content.LibraryContentProvider;
 import org.opencds.cqf.cql.evaluator.dagger.CqlEvaluatorComponent;
 import org.opencds.cqf.cql.evaluator.dagger.DaggerCqlEvaluatorComponent;
 
 import ca.uhn.fhir.context.FhirVersionEnum;
+import org.opencds.cqf.cql.evaluator.engine.retrieve.BundleRetrieveProvider;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -98,7 +105,143 @@ public class CqlCommand implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
 
+
         FhirVersionEnum fhirVersionEnum = FhirVersionEnum.valueOf(fhirVersion);
+
+        TypedRetrieveProviderFactory factory;
+        Triple<String, ModelResolver, RetrieveProvider> customDataProvider = null;
+        RetrieveProvider retrieveProvider = null;
+
+        String resource = "{\n" +
+                "    \"_id\" : \"62691f658791290e3cba0949\",\n" +
+                "    \"resourceType\" : \"Bundle\",\n" +
+                "    \"id\" : \"108314\",\n" +
+                "    \"entry\" : [ \n" +
+                "        [ \n" +
+                "            {\n" +
+                "                \"resource\" : {\n" +
+                "                    \"resourceType\" : \"Patient\",\n" +
+                "                    \"id\" : \"108314\",\n" +
+                "                    \"name\" : {\n" +
+                "                        \"given\" : [ \n" +
+                "                            \"FirstName\"\n" +
+                "                        ],\n" +
+                "                        \"family\" : \"LastName\"\n" +
+                "                    },\n" +
+                "                    \"gender\" : \"male\",\n" +
+                "                    \"birthDate\" : \"2006-06-19\",\n" +
+                "                    \"identifier\" : [ \n" +
+                "                        {\n" +
+                "                            \"use\" : \"usual\",\n" +
+                "                            \"type\" : [],\n" +
+                "                            \"system\" : \"\",\n" +
+                "                            \"value\" : \"108314\",\n" +
+                "                            \"assigner\" : {\n" +
+                "                                \"display\" : \"Exchange HMO\"\n" +
+                "                            }\n" +
+                "                        }\n" +
+                "                    ],\n" +
+                "                    \"_class\" : \"com.example.springjsontofhirparser.FhirResources.Patient\"\n" +
+                "                },\n" +
+                "                \"_class\" : \"com.example.springjsontofhirparser.FhirResources.ResourceChild\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"resource\" : {\n" +
+                "                    \"resourceType\" : \"Coverage\",\n" +
+                "                    \"id\" : \"108314\",\n" +
+                "                    \"identifier\" : [ \n" +
+                "                        {\n" +
+                "                            \"use\" : \"usual\",\n" +
+                "                            \"type\" : [],\n" +
+                "                            \"system\" : \"\",\n" +
+                "                            \"value\" : \"108314\",\n" +
+                "                            \"assigner\" : {\n" +
+                "                                \"display\" : \"Exchange HMO\"\n" +
+                "                            }\n" +
+                "                        }\n" +
+                "                    ],\n" +
+                "                    \"type\" : {\n" +
+                "                        \"coding\" : [ \n" +
+                "                            {\n" +
+                "                                \"system\" : \"urn:oid:HEDIS.Commercial.Custom.Codes.22\",\n" +
+                "                                \"version\" : \"\",\n" +
+                "                                \"code\" : \"MMO\",\n" +
+                "                                \"display\" : \"Commercial Custom code\"\n" +
+                "                            }\n" +
+                "                        ]\n" +
+                "                    },\n" +
+                "                    \"policyHolder\" : {\n" +
+                "                        \"reference\" : \"Patient/108314\"\n" +
+                "                    },\n" +
+                "                    \"subscriber\" : {\n" +
+                "                        \"reference\" : \"Patient/108314\"\n" +
+                "                    },\n" +
+                "                    \"beneficiary\" : {\n" +
+                "                        \"reference\" : \"Patient/108314\"\n" +
+                "                    },\n" +
+                "                    \"payor\" : {\n" +
+                "                        \"reference\" : \"Organization/Exchange HMO\"\n" +
+                "                    },\n" +
+                "                    \"period\" : {\n" +
+                "                        \"start\" : \"2012-04-03\",\n" +
+                "                        \"end\" : \"2023-12-31\"\n" +
+                "                    },\n" +
+                "                    \"order\" : 0,\n" +
+                "                    \"_class\" : \"com.example.springjsontofhirparser.FhirResources.Coverage\"\n" +
+                "                },\n" +
+                "                \"_class\" : \"com.example.springjsontofhirparser.FhirResources.ResourceChild\"\n" +
+                "            }, \n" +
+                "            {\n" +
+                "                \"resource\" : {\n" +
+                "                    \"resourceType\" : \"Encounter\",\n" +
+                "                    \"id\" : \"108314\",\n" +
+                "                    \"identifier\" : [],\n" +
+                "                    \"status\" : \"finished\",\n" +
+                "                    \"class\" : {\n" +
+                "                        \"system\" : \"urn:oid:\",\n" +
+                "                        \"version\" : \"\",\n" +
+                "                        \"code\" : \"PersiviaCode\",\n" +
+                "                        \"display\" : \"\"\n" +
+                "                    },\n" +
+                "                    \"type\" : [ \n" +
+                "                        {\n" +
+                "                            \"coding\" : [ \n" +
+                "                                {\n" +
+                "                                    \"system\" : \"urn:oid:\",\n" +
+                "                                    \"version\" : \"\",\n" +
+                "                                    \"code\" : \"PersiviaCode\",\n" +
+                "                                    \"display\" : \"\"\n" +
+                "                                }\n" +
+                "                            ]\n" +
+                "                        }\n" +
+                "                    ],\n" +
+                "                    \"subject\" : {\n" +
+                "                        \"reference\" : \"Patient/108314\"\n" +
+                "                    },\n" +
+                "                    \"diagnosis\" : [],\n" +
+                "                    \"participant\" : [],\n" +
+                "                    \"period\" : {\n" +
+                "                        \"start\" : \"2022-01-01\",\n" +
+                "                        \"end\" : \"2022-01-01\"\n" +
+                "                    },\n" +
+                "                    \"serviceProvider\" : {\n" +
+                "                        \"reference\" : \"Practitioner/UNK001\"\n" +
+                "                    },\n" +
+                "                    \"_class\" : \"com.example.springjsontofhirparser.FhirResources.Encounter\"\n" +
+                "                },\n" +
+                "                \"_class\" : \"com.example.springjsontofhirparser.FhirResources.ResourceChild\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    ],\n" +
+                "    \"_class\" : \"com.example.springjsontofhirparser.FhirResources.Bundle\"\n" +
+                "}";
+        IBaseBundle bundle;
+        FhirContext fhirContext = fhirVersionEnum.newContext();
+        IParser selectedParser = fhirContext.newJsonParser();
+        bundle = (IBaseBundle) selectedParser.parseResource(resource);
+        retrieveProvider = new BundleRetrieveProvider(fhirContext, bundle);
+
+
 
         CqlEvaluatorComponent cqlEvaluatorComponent = DaggerCqlEvaluatorComponent.builder()
                 .fhirContext(fhirVersionEnum.newContext()).build();
@@ -146,6 +289,11 @@ public class CqlCommand implements Callable<Integer> {
             else {
                 dataProvider = dataProviderFactory.create(new EndpointInfo().setType(Constants.HL7_FHIR_FILES_CODE));
             }
+
+            //Changes need to be made here
+
+
+            BundleRetrieveProvider bundleRetrieveProvider = (BundleRetrieveProvider) dataProvider.getRight();
 
             cqlEvaluatorBuilder.withModelResolverAndRetrieveProvider(dataProvider.getLeft(), dataProvider.getMiddle(),
                     dataProvider.getRight());
