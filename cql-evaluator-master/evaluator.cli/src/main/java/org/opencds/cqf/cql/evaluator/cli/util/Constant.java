53925206a6f9a9cf5f3c7a2bf8e40c8277a5fab0
Azhar <azhar0287@gmail.com>
2022-06-07 14:31:39 +0500

    minor change

--- cql-evaluator-master/evaluator.cli/src/main/java/org/opencds/cqf/cql/evaluator/cli/util/Constant.java
+++ cql-evaluator-master/evaluator.cli/src/main/java/org/opencds/cqf/cql/evaluator/cli/util/Constant.java
@@ -4,10 +4,9 @@ public class Constant {
    public static final String CODE_TYPE_COMMERCIAL = "HEDIS.Commercial.Custom.Codes.22";
    public static final  String CODE_TYPE_MEDICAID = "HEDIS.Medicaid.Custom.Codes.22";
    public static final String CODE_TYPE_MEDICARE = "HEDIS.Medicare.Custom.Codes.22";
-   public static final String FOLDER_NAME="/CCS_HEDIS_MY2022";
-   private static final String testResourceRelativePath = "resources";
-
-
+   public static final String FOLDER_NAME="CCS_HEDIS_MY2022";
+//   private static final String testResourceRelativePath = "resources";  //for jar
+   private static final String testResourceRelativePath = "evaluator.cli/src/main/resources";
 
    /*Variables of Engine*/
    public static final String FHIR_VERSION = "R4";
